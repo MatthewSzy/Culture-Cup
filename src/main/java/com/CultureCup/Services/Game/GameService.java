f8ae8e07ccb4c059ea80a14722108893d4c83139
MatthewSzy <matthewszymone@gmail.com>
2021-11-21 12:59:27 +0100

    Security Fix

--- src/main/java/com/CultureCup/Services/Game/GameService.java
+++ src/main/java/com/CultureCup/Services/Game/GameService.java
@@ -53,7 +53,7 @@ public class GameService {
 
     public List<GameListItem> getHomePopular() {
 
-        JSONArray jsonArray = new JSONArray(HttpRequestClass.sendPostRequestToIGDB(getGameFirstPartURL, "fields cover.url,name,rating,rating_count,release_dates.date; sort rating desc; where rating != null & cover != null & rating_count > 50 & release_dates != null & release_dates.date != null; limit 20; offset 20;", accessToken).body());
+        JSONArray jsonArray = new JSONArray(HttpRequestClass.sendPostRequestToIGDB(getGameFirstPartURL, "fields cover.url,name,rating,rating_count,release_dates.date; where id > 1000 & cover != null & release_dates != null & release_dates.date != null; limit 20; offset 20;", accessToken).body());
 
         List<GameListItem> gameListItem = new ArrayList<>();
         for (int i = 0; i < 4; i++) gameListItem.add(ObjectCreateClass.CreateGameListItem(jsonArray.getJSONObject(i)));
